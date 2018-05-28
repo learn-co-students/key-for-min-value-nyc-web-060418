2432ad24b76a730d700170c7ec6ed852c72d4a09
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-27 23:06:36 -0400

    implemented find min method

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil
+  end
+  key_of_current_min, current_min = name_hash.first
+  name_hash.each do |key, value|
+    if value < current_min
+      current_min = value
+      key_of_current_min = key
+    end
+  end
+  return key_of_current_min
 end
